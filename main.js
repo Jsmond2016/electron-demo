492888ea76105781499e0f177782434cf01bea15
24517 <jin.huang02@hand-china.com>
2019-10-22 17:04:19 +0800

    代码重构优化

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true //设置这个可以使用 nodejs 的API
+class AppWindow extends BrowserWindow{
+  constructor(config, fileLocation) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true //设置这个可以使用 nodejs 的API
+      }
     }
-  })
 
-  mainWindow.loadFile("./renderer/index.html")
+    //const finalConfig = Object.assign(basicConfig, config) // ES5写法 用后面的覆盖前面的
+    const finalConfig = { ...basicConfig, ...config } // ES6语法，效果等同上面
+    super(finalConfig)
+    this.loadFile(fileLocation)
+  }
+}
+
+
+
+
+
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html')
   
   ipcMain.on('add-music-window', (event, args) => {
-    const addWindow = new BrowserWindow({
+    const addWindow = new AppWindow({
       width: 500,
       height: 400,
       webPreferences: {
         nodeIntegration: true //设置这个可以使用 nodejs 的API
       },
       parent: mainWindow
-    })
-    addWindow.loadFile('./renderer/add.html')
+    }, './renderer/add.html')
   })
 
 })
